54c676df9f6e802165614b2217d42384301206b9
Zdenko Abarca <eabarcac004@alumno.uaemex.mx>
2025-10-02 21:22:13 -0600

    Container

---
+++ app/(routes)/about-me/page.tsx
@@ -0,0 +1,14 @@
+import { ContainerPage } from "@/components/container";
+import { TransitionPage } from "@/components/transition-page";
+
+export default function PageAboutMe() {
+  return (
+    <>
+        <TransitionPage />
+        <ContainerPage>
+            <p>About Me</p>
+        </ContainerPage>
+    </>
+  )
+}
+    
